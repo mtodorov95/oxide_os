a42661f6a74c8a8791895e70a200740c34401144
Mario Todorov <mtodorov95@gmail.com>
2023-12-02 18:34:34 +0200

    Add testing framework
    Implement support for tests
    Implement kernel->host communication via serial port
    Moved shared code to lib.rs
    Implemented integration tests

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,24 @@
 #![no_main]
 // Enable testing in a no std environment
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(oxide_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-
-mod vga_buffer;
+use oxide_os::println;
 
 // Called on panic
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    oxide_os::test_panic_handler(info)
 }
 
 // Preserves the actual name of the fundtion when going through the compiler
@@ -28,13 +35,13 @@ fn panic(info: &PanicInfo) -> ! {
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
     loop {}
 }
 
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
