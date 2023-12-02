d51de43f951cdf78eaa801ff6f9c5ca47d15807a
Mario Todorov <mtodorov95@gmail.com>
2023-12-02 11:59:31 +0200

    Turn the freestanding binary into a mini kernel
    Create custom target json
    Compile Rust core for our target
    Write msg to VGA buffer
    Use bootimage to compile our kernel + bootloader crate and link them together into a bootable image

---
+++ Readme.md
@@ -0,0 +1,20 @@
+In order to create a bootable image:
+- Install bootimage
+```
+cargo install bootimage
+```
+- Add llvm-tools-preview
+```
+rustup component add llvm-tools-preview
+```
+- Create the disk image
+```
+cargo bootimage
+```
+
+Then use it in:
+- QEMU
+```
+> qemu-system-x86_64 -drive format=raw,file=target/x86_64-oxide_os/debug/bootimage-oxide_os.bin
+```
+- Write to USB and run on a real machine
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-// Add and compile for target (host by default) that doesn't have an
+// Compile for custom target (host by default) that doesn't have an
 // underlying OS. That way the linker won't assume that there is an underlying
 // C runtime.
-// The following is an embedded ARM system 
-// rustup target add thumbv7em-none-eabihf
-// cargo build --target thumbv7em-none-eabihf
+// cargo build --target x86_64-oxide_os.json
 
-
+// Don't link the standard library
 #![no_std]
 // Overwrite the standard entry point chain e.g. crt0 -> start > main
 #![no_main]
@@ -18,9 +16,24 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
 // Preserves the actual name of the fundtion when going through the compiler
 // extern "C" - use the C calling convention instead
+// Linker looks for fn named _start
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    // Get raw pointer
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        // Write the string byte and color byte (0xb) to the vga_buffer
+        // Unsafe, because Rust can't prove that a pointer we created is valid
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
